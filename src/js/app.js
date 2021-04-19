c13a2c3a57bbdcaddd0073e2cca53abff1d740d5
napastyuk <napastyuk@gmail.com>
2021-04-19 03:44:26 +0300

    add tooltip full config

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 var SpriteSpin = require("spritespin");
 import { SVG } from '@svgdotjs/svg.js';
-import content from "./content.json";
+import config from "./config.json";
 
 window.onload = function () {
   $(function () {
@@ -30,6 +30,7 @@ function initSlider(tooltipArr) {
       updateTooltips(tooltipArr, data.frame);
     },
     onFrame: function (e, data) {
+      // console.log('текущий кадр', data.frame)
       //поступил запрос на изменение текущего кадра
       updateTooltips(tooltipArr, data.frame);
     },
@@ -51,98 +52,103 @@ function drowSVGTooltips() {
     .size("100", "100")
     .viewbox("0 0 100 100");
 
-  let restoranTooltip = dropTooltipItem(draw);
+    //dev ellips
+  // var ellipse = draw.ellipse().radius(13, 1).fill('rgba(255, 0, 102, 0.5)').center(50, 47)
 
   let tooltipArray = [];
-  tooltipArray.push(restoranTooltip);
+  for (var tooltipItem in config) {
+    let restoranTooltip = dropTooltipItem(draw, config[tooltipItem]);
+    tooltipArray.push(restoranTooltip);
+  }
+
   return tooltipArray;
 }
 
-function dropTooltipItem(draw) {
+function dropTooltipItem(draw, tooltipConfig) {
   //константы
-  const targetX = 56; //куда указывать X
-  const targetY = 29; //куда указывать Y
-  const colorName = "rgba(0, 0, 0, 0.2)";
-
-  let content = "Видовой ресторан"; // \n для переноса строки
+  const colorName ="rgba(0, 0, 0, 0.2)";
   const textStyleObj = {
     fill: "#fff",
     family: "system-ui, serif",
     size: "2px",
   };
 
-  //для каждого тултипа
-  const nestedGroup = draw
-    .group()
-    .attr("data-place", "restaurant")
+  //создадим группу для прямоугольника с текстом
+  const nestedGroup = draw.group()
+    .attr("data-place", tooltipConfig.slug)
     .css("pointer-events", "auto")
     .click(function (e) {
       openPopup(e.target.closest("g").dataset.place);
     });
   const rect = nestedGroup.rect("20", "5").radius(1).attr({ fill: colorName }); // прямоугольник для фона, размеры пока захардкожены(!) что бы не вводить функцию вычисления размера текста
   const text = nestedGroup
-    .text(content)
+    .text(tooltipConfig.content.tooltipTitle)  // \n для переноса строки
     .font(textStyleObj)
     .css("user-select", "none"); //вставляем контент и добавляем стили для текста
   //text.cx(rect.rbox().cx).cy(rect.rbox().cy); //центрируем получившийся текст , пока не работает
-  text.dmove(1, 0.5);
-  nestedGroup.move(15, 15); //переместим тултип поближе к зданию
-
-  let nearestCorner = getNearestCorner(nestedGroup, targetX, targetY);
+  text.dmove(1, 0.5); //коррекция надписи в прямоугольнике по высоте
+  nestedGroup.move(tooltipConfig.defaultCoord.tooltipX, tooltipConfig.defaultCoord.tooltipY); //переместим тултип поближе к зданию
+  
   //добавляем указывающую линию до нужной точки
+  //let nearestCorner = getNearestCorner(nestedGroup, tooltipConfig.defaultCoord.targetX, tooltipConfig.defaultCoord.targetY);  не иммет смысла т к в момент загрузки все размеры равны нулю
   const pointLine = draw
-    .line(34.7, 19.7, 56, 29)
+    .line(1, 1, tooltipConfig.defaultCoord.targetX, tooltipConfig.defaultCoord.targetY) //пока нарисуем от угла, в момент загрузки все равно обновим
     .stroke(colorName)
     .attr({ "stroke-linecap": "round", "stroke-width": 0.4 });
 
-  return { tooltipEl: nestedGroup, lineEl: pointLine };
+  return { tooltipEl: nestedGroup, lineEl: pointLine, config: tooltipConfig};
 }
 
-function updateTooltips(tooltipArr, frame) {
-  let currentTooltip = tooltipArr[0];
-  //todo: сделать обход по массиву tooltipArr
-  if ((frame >= 0 && frame < 48) || (frame > 76 && frame < 100)) {
-    //кадры во время которых показывать тултип
-    currentTooltip.tooltipEl.show();
-    currentTooltip.lineEl.show();
-    let target = getCoordOnEllips(13, 2, angles[frame]); //12 и 2 радиусы описывающий конкретный элипс по которому движется лииния тултипа
-    let nearestCorner = getNearestCorner(
-      currentTooltip.tooltipEl,
-      target.x,
-      target.y
-    );
-    currentTooltip.lineEl.plot(
-      nearestCorner.x,
-      nearestCorner.y,
-      target.x,
-      target.y
-    );
-  } else {
-    currentTooltip.tooltipEl.hide();
-    currentTooltip.lineEl.hide();
-  }
+function updateTooltips(tooltipArr, currentFrame) {
+  tooltipArr.forEach(currentTooltip => {
+    let startFr = currentTooltip.config.targetFrameSet.startRange;
+    let endFr = currentTooltip.config.targetFrameSet.endRange;
+    if ((currentFrame >= 0 && currentFrame < startFr) || (currentFrame > endFr && currentFrame <= 100)) {
+      //кадры во время которых показывать тултип
+      currentTooltip.tooltipEl.show();
+      currentTooltip.lineEl.show();
+      let elpSizes = currentTooltip.config.targetEllipsSizes;
+      let target = getCoordOnEllips(elpSizes.x, elpSizes.y, angles[currentFrame], elpSizes.s , elpSizes.h ); //12 и 2 радиусы описывающий конкретный элипс по которому движется лииния тултипа
+      let nearestCorner = getNearestCorner(
+        currentTooltip.tooltipEl,
+        target.x,
+        target.y
+      );
+      currentTooltip.lineEl.plot(
+        nearestCorner.x,
+        nearestCorner.y,
+        target.x,
+        target.y
+      );
+    } else {
+      //иначе скрываем
+      currentTooltip.tooltipEl.hide();
+      currentTooltip.lineEl.hide();
+    }
+  });
 }
 
-function getCoordOnEllips(a, b, deg) {
+function getCoordOnEllips(a, b, deg, shift ,height) {
+  deg=deg+shift;
   let rad = (deg * Math.PI) / 180;
   return {
     x: a * Math.cos(rad) + 50, //50 потому что от центра
-    y: b * Math.sin(rad) + 28, //27 высота элипса траектории для конкретного тултипа
+    y: b * Math.sin(rad) + height, //27 высота элипса траектории для конкретного тултипа
   };
 }
 
 function openPopup(popupName) {
-  if (content.hasOwnProperty(popupName)) {
+  if (config.hasOwnProperty(popupName)) {
     let popUpWrapper = document.querySelector("#popup");
 
     let popUpHeader = popUpWrapper.querySelector(".popup__header");
-    popUpHeader.textContent = content[popupName].header;
+    popUpHeader.textContent = config[popupName].content.header;
 
     let popUpImg = popUpWrapper.querySelector(".popup__img > img");
-    popUpImg.src = content[popupName].imgPath;
+    popUpImg.src = config[popupName].content.imgPath;
 
     let popUpContent = popUpWrapper.querySelector(".popup__text>p");
-    popUpContent.textContent = content[popupName].content;
+    popUpContent.textContent = config[popupName].content.content;
 
     popUpWrapper.hidden = false;
   } else {
@@ -152,6 +158,7 @@ function openPopup(popupName) {
 
 //хелпер - вычисляем ближайший угол прямоугольника к целевой точке
 function getNearestCorner(rectangleEl, targetX, targetY) {
+
   //занесём в переменные координаты углов прямоугольника и целевой точки
   let topLeft = { x: rectangleEl.bbox().x, y: rectangleEl.bbox().y };
   let topRight = { x: rectangleEl.bbox().x2, y: rectangleEl.bbox().y };
@@ -311,12 +318,4 @@ const angles = [
   32,
 ];
 
-//контент для тултипов
-// const content = {
-//   "restaurant": {
-//     header: "Видовой ресторан",
-//     imgPath: "img/tooltip-img/restaurant.jpg",
-//     content:
-//       "Кафе и рестораны различного класса. Для сотрудников многочисленных офисов будут организованы недорогие столовые.",
-//   },
-// };
+
